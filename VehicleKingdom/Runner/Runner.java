3d2f1bbb12855e1c741edb023455beece0c36b05
jbeatty2019 <jbeatty2019@pingry.org>
2017-02-24 08:11:56 -0500

    First commit

---
+++ VehicleKingdom/Runner/Runner.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Runner here.
+ * 
+ * @author (Joseph Beatty, Nathan Hefner, Jack Gambello, Ethan Malzberg) 
+ * @version (1.0)
+ */
+public class Runner
+{
+   
+    public static void main(String[] args)
+    {
+    Bike a = new Bike();
+    
+    System.out.println(a);
+    
+    Truck b = new Truck();
+    
+    System.out.println(b);
+    }
+
+}
